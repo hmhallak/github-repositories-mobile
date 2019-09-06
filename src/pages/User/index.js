79cd80dea51ef623b73551e88db35797820ea866
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-06 00:30:26 -0400

    Update index.js
    adds stars loading with infinite scroll in user profile

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -30,17 +30,37 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    page: 1,
     loading: true,
   };
 
   async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
 
-    this.setState({ stars: response.data, loading: false });
-  }
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+    });
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.load(nextPage);
+  };
 
   render() {
     const { navigation } = this.props;
@@ -60,6 +80,8 @@ export default class User extends Component {
           <Loading />
         ) : (
           <Stars
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             data={stars}
             keyExtractor={star => String(star.id)}
             renderItem={({ item }) => (
